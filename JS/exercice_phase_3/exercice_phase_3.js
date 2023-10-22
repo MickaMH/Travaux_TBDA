3f03f6b6369b447f89a91a119472d54193490562
MickaMH <145990342+MickaMH@users.noreply.github.com>
2023-10-22 19:00:07 +0200

    Add files via upload

---
+++ JS/exercice_phase_3/exercice_phase_3.js
@@ -0,0 +1,3 @@
+alert("<b>Coucou !</b>"); /*Je remarque que ça ne marche pas, les balises sont écrites.*/
+            console.log("Ah que Coucou !");
+            document.write("<h1>Mon premier script</h1>");
