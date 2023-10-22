c44a1cc25c0eb3b13aba0472084a934349b8a710
MickaMH <145990342+MickaMH@users.noreply.github.com>
2023-10-22 19:02:24 +0200

    Delete JS/exercice1.js

--- JS/exercice1.js
+++
@@ -1,3 +0,0 @@
-alert("<b>Coucou !</b>"); /*Je remarque que ça ne marche pas, les balises sont écrites.*/
-            console.log("Ah que Coucou !");
-            document.write("<h1>Mon premier script</h1>");
